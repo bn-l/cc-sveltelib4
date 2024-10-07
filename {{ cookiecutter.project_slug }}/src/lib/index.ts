e7b4fe5bac78cd562545cbad67b853dd9fde53aa
bn-l <13162197+bn-l@users.noreply.github.com>
2024-10-07 15:09:27 +1100

    added example import function for testing

--- {{ cookiecutter.project_slug }}/src/lib/index.ts
+++ {{ cookiecutter.project_slug }}/src/lib/index.ts
@@ -1,1 +1,6 @@
 // Reexport your entry components here
+
+export function helloer() {
+    return "hello";
+}
+
--- {{ cookiecutter.project_slug }}/src/routes/+page.svelte
+++ {{ cookiecutter.project_slug }}/src/routes/+page.svelte
@@ -1,3 +1,17 @@
 <h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+    Create your package using @sveltejs/package and preview/showcase your work
+    with SvelteKit
+</p>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<p>
+    And here's your greeting: {greeting}
+</p>
+
+<script lang="ts">
+    import { helloer } from "$lib/index.ts";
+
+    let greeting = helloer();
+</script>
